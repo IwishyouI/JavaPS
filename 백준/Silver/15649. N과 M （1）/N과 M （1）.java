4dc10dafd535e9ab1475ce154ce3bb2669b49e75
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-04-24 22:48:58 +0900

    [Silver III] Title: N과 M (1), Time: 1740 ms, Memory: 64032 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,55 @@
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static int N, M;
-    static int[] arr;
-    static boolean[] visited;
+    static int arr[];
+    static boolean visited[];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        arr = new int[M];        // 수열을 저장할 배열
-        visited = new boolean[N+1]; // 방문 여부 체크
+        String[] split = bf.readLine().split(" ");
+
+         N = Integer.parseInt(split[0]);
+         M = Integer.parseInt(split[1]);
+
+        arr = new int[M];
+
+        visited = new boolean[N+1];
 
         dfs(0);
-        sc.close();
+
+
     }
 
-    // 간단한 DFS로 구현
     static void dfs(int depth) {
         if (depth == M) {
             for (int val : arr) {
-                System.out.print(val + " ");
-
+                System.out.print(val+ " ");
             }
             System.out.println();
             return;
         }
+
+
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 arr[depth] = i;
                 dfs(depth + 1);
                 visited[i] = false;
+
             }
         }
-
     }
+
 }

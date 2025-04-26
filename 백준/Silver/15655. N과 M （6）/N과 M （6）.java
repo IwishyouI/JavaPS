8a2b7c6f101c85eca9ff0fc0843588cadf7a41b8
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-04-26 09:20:12 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,48 +1,58 @@
-import java.util.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static int N, M;
-    static int[] nums;
-    static int[] selected;
+    static int arr[];
+    static boolean visited[];
     static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        N = sc.nextInt();
-        M = sc.nextInt();
-        
-        nums = new int[N];
-        selected = new int[M];
-        
-        for (int i = 0; i < N; i++) {
-            nums[i] = sc.nextInt();
+
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = bf.readLine().split(" ");
+
+        String[] secondLine = bf.readLine().split(" ");
+
+        numbers = new int[secondLine.length];
+
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
+
+        for (int i = 0; i < secondLine.length; i++) {
+            numbers[i] = Integer.parseInt(secondLine[i]);
         }
-        
-        // 수를 오름차순으로 정렬
-        Arrays.sort(nums);
-        
-        // 백트래킹 시작
-        backtrack(0, 0);
-        
-        System.out.print(sb);
-        sc.close();
+        Arrays.sort(numbers);
+        arr = new int[M];
+        dfs(0, 0);
+
+        System.out.println(sb);
+
     }
-    
-    static void backtrack(int start, int depth) {
-        // M개의 수를 모두 선택했다면 결과 저장
+
+    static void dfs(int start, int depth) {
         if (depth == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(selected[i]).append(" ");
+            for (int val : arr) {
+                sb.append(val).append(" ");
             }
             sb.append("\n");
             return;
         }
-        
-        // 현재 위치부터 끝까지 순회하며 수를 선택
+
+
         for (int i = start; i < N; i++) {
-            selected[depth] = nums[i];
-            backtrack(i + 1, depth + 1);
+            arr[depth] = numbers[i];
+            dfs(i+1, depth + 1);
         }
     }
+
 }

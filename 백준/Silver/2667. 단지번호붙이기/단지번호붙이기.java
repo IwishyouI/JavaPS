167836bfdf2ab6d8428ad8dfa9a87a59143c6dcc
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-06-18 21:06:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,65 +1,92 @@
+
+
+import java.io.*;
 import java.util.*;
 
 public class Main {
+
+
     static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0}; // 상하좌우 이동을 위한 배열
-    static int[] dy = {0, 0, -1, 1};
-    static ArrayList<Integer> complexSizes = new ArrayList<>(); // 단지별 집의 수를 저장할 리스트
-    
+
+    static int dx[] = {0, 0, -1, 1};
+    static int dy[] = {-1, 1, 0, 0};
+    static boolean visited[][];
+    static int arr[][];
+
+    static List<Integer> aptSize = new ArrayList<>();
+
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        
-        N = sc.nextInt();
-        map = new int[N][N];
+
+       N = sc.nextInt();
+
+        arr = new int[N][N];
         visited = new boolean[N][N];
-        
-        // 지도 입력 받기
+
+
+
         for (int i = 0; i < N; i++) {
             String line = sc.next();
             for (int j = 0; j < N; j++) {
-                map[i][j] = line.charAt(j) - '0';
+                arr[i][j] = line.charAt(j) - '0';
             }
         }
-        
-        // 모든 위치를 확인하며 단지 찾기
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    int size = dfs(i, j);
-                    complexSizes.add(size);
+                if (arr[i][j] == 1 && !visited[i][j]) {
+                    int num = bfs(i, j);
+                    aptSize.add(num);
                 }
             }
         }
-        
-        // 단지 내 집의 수 오름차순 정렬
-        Collections.sort(complexSizes);
-        
-        // 결과 출력
-        System.out.println(complexSizes.size()); // 총 단지 수
-        for (int size : complexSizes) {
-            System.out.println(size); // 각 단지 내 집의 수
+
+        Collections.sort(aptSize);
+        System.out.println(aptSize.size());
+        for (int val : aptSize) {
+            System.out.println(val);
         }
-        
-        sc.close();
     }
-    
-    // DFS를 통해 하나의 단지를 탐색하고 집의 수를 반환
-    static int dfs(int x, int y) {
+
+
+    static int bfs(int x, int y) {
+
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[]{x, y});
         visited[x][y] = true;
-        int count = 1; // 현재 집 포함
-        
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            // 지도 범위 내에 있고, 집이 있으며, 아직 방문하지 않은 경우
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N && map[nx][ny] == 1 && !visited[nx][ny]) {
-                count += dfs(nx, ny); // 연결된 집 탐색 후 집의 수 누적
+        int count = 1;
+
+
+        while (!queue.isEmpty()) {
+
+
+            int[] node = queue.poll();
+
+            int nowX = node[0];
+            int nowY = node[1];
+
+
+            for (int i = 0; i < 4; i++) {
+
+                int nextX = nowX + dx[i];
+                int nextY = nowY + dy[i];
+
+                if (nextX < N && nextY < N && nextX >= 0 && nextY >= 0 && !visited[nextX][nextY] && arr[nextX][nextY] == 1) {
+
+                    visited[nextX][nextY] = true;
+                    arr[nextX][nextY] = arr[nowX][nowY] + 1;
+                    queue.add(new int[]{nextX, nextY});
+                    count += 1;
+                }
             }
         }
-        
         return count;
     }
+
 }
+
+

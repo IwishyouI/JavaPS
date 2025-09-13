0aacbb74da88526de61289b94bfb05f504e7ab96
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-09-13 10:30:48 +0900

    [Silver I] Title: 나이트의 이동, Time: 240 ms, Memory: 65128 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,9 +1,9 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
 
 public class Main {
 
@@ -11,75 +11,59 @@ public class Main {
     static final int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
 
     public static void main(String[] args) throws IOException {
-
-
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(bf.readLine());
 
+        int N = Integer.parseInt(bf.readLine()); // 테스트 케이스 개수
 
         while (N-- > 0) {
+            int T = Integer.parseInt(bf.readLine()); // 체스판 크기
 
-            int M = Integer.parseInt(bf.readLine().trim());
-            StringTokenizer st = new StringTokenizer(bf.readLine().trim());
+            String[] split = bf.readLine().split(" ");
+            int startX = Integer.parseInt(split[0]);
+            int startY = Integer.parseInt(split[1]);
 
-            int startX = Integer.parseInt(st.nextToken());
-            int startY = Integer.parseInt(st.nextToken());
+            String[] split2 = bf.readLine().split(" ");
+            int targetX = Integer.parseInt(split2[0]);
+            int targetY = Integer.parseInt(split2[1]);
 
-
-            st = new StringTokenizer(bf.readLine().trim());
-
-            int targetX = Integer.parseInt(st.nextToken());
-            int targetY = Integer.parseInt(st.nextToken());
-
-
-            int result = bfs(M, startX, startY, targetX, targetY);
-            sb.append(result).append('\n');
+            int result = bfs(T, startX, startY, targetX, targetY);
+            sb.append(result).append("\n");
         }
 
-        System.out.println(sb);
-
-
+        System.out.print(sb);
     }
 
-
-    static int bfs(int M, int startX, int startY, int targetX, int targetY) {
-        if (startX == targetX && startY == targetY) {
-            return 0;
-        }
+    static int bfs(int N, int startX, int startY, int targetX, int targetY) {
         Queue<Point> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[M][M];
-
-        queue.add(new Point(startX, startY, 0));
+        boolean[][] visited = new boolean[N][N];
 
         visited[startX][startY] = true;
+        queue.add(new Point(startX, startY, 0));
 
         while (!queue.isEmpty()) {
-
             Point p = queue.poll();
 
-            for (int i = 0; i < 8; i++) {
+            // 목표 지점 도착
+            if (p.x == targetX && p.y == targetY) {
+                return p.d;
+            }
 
+            for (int i = 0; i < 8; i++) {
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
 
-                if (nx >= 0 && ny >= 0 && nx < M && ny < M && !visited[nx][ny]) {
-                    if (nx == targetX && ny == targetY) {
-                        return p.d + 1;
-                    }
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[nx][ny]) {
                     visited[nx][ny] = true;
                     queue.add(new Point(nx, ny, p.d + 1));
                 }
             }
         }
-        return -1;
+        return -1; // 도달 불가능한 경우
     }
 
-
     static class Point {
-
         int x, y, d;
-
         public Point(int x, int y, int d) {
             this.x = x;
             this.y = y;
@@ -87,17 +71,3 @@ public class Main {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-

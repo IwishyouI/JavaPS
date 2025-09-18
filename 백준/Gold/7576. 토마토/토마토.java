82a612d68fff2d685fe79b29e2294bd6785ec2b5
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-09-18 15:39:03 +0900

    [Gold V] Title: 토마토, Time: 1544 ms, Memory: 356068 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,41 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-
-    static int N, M;
-
-
-    static int dx[] = {0, 0, -1, 1};
-    static int dy[] = {-1, 1, 0, 0};
-
-    static int arr[][];
-
-    static int days[][];
-
-
+    static int M, N;
+    static int[][] box;
+    static int[][] days;
     static Queue<int[]> queue = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
+    // 상, 하, 좌, 우 이동 방향
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         M = sc.nextInt();
         N = sc.nextInt();
-
-
-        arr = new int[N][M];
-
+        box = new int[N][M];
         days = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                arr[i][j] = sc.nextInt();
-                if (arr[i][j] == 1) {
+                box[i][j] = sc.nextInt();
+                if (box[i][j] == 1) {
                     queue.add(new int[]{i, j});
                 }
             }
@@ -44,53 +29,37 @@ public class Main {
         bfs();
 
         int result = 0;
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0) {
+                if (box[i][j] == 0) {
                     System.out.println(-1);
                     return;
                 }
-
                 result = Math.max(result, days[i][j]);
             }
         }
 
         System.out.println(result);
-
     }
 
-
     static void bfs() {
-
-
         while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
 
-            int[] node = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
 
-            int nowX = node[0];
-            int nowY = node[1];
-
-
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < M) {
-
-                    if (arr[nextX][nextY] == 0) {
-                        arr[nextX][nextY] = 1;
-                        days[nextX][nextY] = days[nowX][nowY] + 1;
-
-                        queue.add(new int[]{nextX, nextY});
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (box[nx][ny] == 0) {
+                        box[nx][ny] = 1; // 익게 만들기
+                        days[nx][ny] = days[x][y] + 1;
+                        queue.add(new int[]{nx, ny});
                     }
                 }
             }
-
         }
     }
-
-
 }
-
-

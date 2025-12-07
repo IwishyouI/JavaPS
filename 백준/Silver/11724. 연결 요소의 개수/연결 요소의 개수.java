f51a6c0c14822a65be4afd668c2803d50428be12
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-12-07 16:19:25 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 740 ms, Memory: 172960 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,56 +1,76 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] graph; // 그래프 표현을 위한 인접 리스트
-    static boolean[] visited; // 방문 여부 체크 배열
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int N, M;
 
-        int N = sc.nextInt(); // 정점의 개수
-        int M = sc.nextInt(); // 간선의 개수
+    static boolean visited[];
+    static ArrayList<Integer>[] graph;
 
-        // 그래프 초기화 (1부터 N까지의 정점)
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
         graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
+        visited = new boolean[N+1];
+
+        for (int i = 0; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        // 간선 정보 입력 받기
         for (int i = 0; i < M; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            // 양방향 간선
-            graph[u].add(v);
-            graph[v].add(u);
+
+            String[] split = bf.readLine().split(" ");
+
+            int x = Integer.parseInt(split[0]);
+            int y = Integer.parseInt(split[1]);
+
+
+            graph[x].add(y);
+            graph[y].add(x);
+
         }
 
-        visited = new boolean[N + 1]; // 방문 배열 초기화
-        int count = 0; // 연결 요소의 개수
+        int count = 0;
 
-        // 모든 정점을 확인
         for (int i = 1; i <= N; i++) {
-            if (!visited[i]) { // 아직 방문하지 않은 정점이라면
-                dfs(i); // DFS 수행
-                count++; // 연결 요소 개수 증가
+
+            if (!visited[i]) {
+                dfs(i);
+                count++;
             }
         }
 
-        // 결과 출력
         System.out.println(count);
-
-        sc.close();
     }
 
-    // DFS 구현
-    static void dfs(int node) {
-        visited[node] = true; // 현재 노드 방문 처리
 
-        // 인접한 노드 중 방문하지 않은 노드를 재귀적으로 방문
-        for (int next : graph[node]) {
+    static void dfs(int x) {
+
+
+        visited[x] = true;
+
+
+        for (int next : graph[x]) {
             if (!visited[next]) {
                 dfs(next);
             }
         }
     }
+
+
 }

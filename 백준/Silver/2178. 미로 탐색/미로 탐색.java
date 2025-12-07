6ea40c1baf89cc671ee97da25ba6bf555111b0ea
지유다 <157937573+IwishyouI@users.noreply.github.com>
2025-12-07 10:48:12 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,54 +6,81 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     static int dx[] = {0, 0, -1, 1};
     static int dy[] = {-1, 1, 0, 0};
     static int N, M;
+
     static int map[][];
     static boolean visited[][];
-    
+
+
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         String line = bf.readLine();
+
         String[] split = line.split(" ");
+
         N = Integer.parseInt(split[0]);
         M = Integer.parseInt(split[1]);
+
+
         visited = new boolean[N][M];
+
         map = new int[N][M];
+
         for (int i = 0; i < N; i++) {
             String m = bf.readLine();
             for (int j = 0; j < M; j++) {
                 map[i][j] = m.charAt(j) - '0';
+
             }
+
         }
-        
-        // ⭐ 추가!
+
+
         bfs(0, 0);
-        System.out.println(map[N-1][M-1]);
+        System.out.println(map[N - 1][M - 1]);
+
     }
-    
-    // ⭐ void로 변경, distance 파라미터 제거
+
+
     static void bfs(int x, int y) {
+
+
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});  // ⭐ 좌표만!
+        queue.add(new int[]{x, y});
         visited[x][y] = true;
-        
+
+
         while (!queue.isEmpty()) {
             int[] node = queue.poll();
+
             int cx = node[0];
             int cy = node[1];
-            // cd 제거!
-            
+
+
             for (int i = 0; i < 4; i++) {
+
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M 
-                    && !visited[nx][ny] && map[nx][ny] == 1) {
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny] && map[nx][ny] == 1) {
+
                     visited[nx][ny] = true;
                     map[nx][ny] = map[cx][cy] + 1;
-                    queue.add(new int[]{nx, ny});  // ⭐ 좌표만!
+                    queue.add(new int[]{nx, ny});
+
                 }
+
             }
+
         }
+
     }
+
+
 }
